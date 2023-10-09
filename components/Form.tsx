4e603803d86a89b63fda1d7fd8ab2eb32065ff95
Vyrolian <107125437+Vyrolian@users.noreply.github.com>
2023-10-09 15:18:37 +0300

    project1

---
+++ components/Form.tsx
@@ -0,0 +1,92 @@
+import React, { ChangeEvent } from "react";
+
+type FormProps = {
+  base: number | null;
+  exp: number | null;
+  mod: number | null;
+  result: number | null;
+  onBaseChange: (value: number | null) => void;
+  onExpChange: (value: number | null) => void;
+  onModChange: (value: number | null) => void;
+  onCalculate: () => void;
+};
+
+export default function Form({
+  base,
+  exp,
+  mod,
+  result,
+  onBaseChange,
+  onExpChange,
+  onModChange,
+  onCalculate,
+}: FormProps) {
+  const handleBaseChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    console.log("Input Value:", value); // Logging the raw input value
+
+    if (value === "") {
+      onBaseChange(null); // Set to null when input is empty
+      console.log("Setting state to null due to empty input"); // Log status
+    } else {
+      const numValue = parseInt(value, 10);
+      console.log("Parsed Number Value:", numValue); // Logging parsed number value
+
+      if (!isNaN(numValue)) {
+        onBaseChange(numValue);
+        console.log("Setting state to:", numValue); // Log the value being set to state
+      }
+    }
+  };
+
+  const handleExpChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+    if (!isNaN(value)) onExpChange(value);
+    else onExpChange(null);
+  };
+
+  const handleModChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+    if (!isNaN(value)) onModChange(value);
+    else onModChange(null);
+  };
+
+  return (
+    <div className="calculator">
+      <input
+        type="text"
+        className="formbox-1"
+        value={base !== null ? base : ""}
+        onChange={handleBaseChange}
+        placeholder="Base"
+      />
+      <span className="operator-1">^</span>
+      <input
+        type="text"
+        className="formbox-2"
+        value={exp || ""}
+        onChange={handleExpChange}
+        placeholder="Exp"
+      />
+      <span className="operator-2">%</span>
+      <input
+        type="text"
+        className="formbox-3"
+        value={mod || ""}
+        onChange={handleModChange}
+        placeholder="Mod"
+      />
+      <button className="equals-button" onClick={onCalculate}>
+        =
+      </button>
+      <input
+        type="text"
+        className="formbox"
+        style={{ width: "70px" }}
+        readOnly
+        value={result !== null ? result.toString() : ""}
+        placeholder="Result"
+      />
+    </div>
+  );
+}
--- components/Project1.tsx
+++ components/Project1.tsx
@@ -3,74 +3,108 @@ import React, { useState, ChangeEvent } from "react";
 import Image from "next/image";
 import myGif from "../images/smolganyu.gif"; // Adjust the path as necessary
 import "./Input.css";
+import Form from "./Form";
 
 export default function Project1() {
-  const [base, setBase] = useState<number | null>(null);
-  const [exponent, setExponent] = useState<number | null>(null);
-  const [modulus, setModulus] = useState<number | null>(null);
-  const [result, setResult] = useState<number | null>(null);
+  const [base1, setBase1] = useState<number | null>(null);
+  const [exp1, setExp1] = useState<number | null>(null);
+  const [mod1, setMod1] = useState<number | null>(null);
+  const [result1, setResult1] = useState<number | null>(null);
 
-  const handleBaseChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(e.target.value);
-    if (!isNaN(value)) setBase(value);
-    else setBase(null);
+  const [base2, setBase2] = useState<number | null>(null);
+  const [exp2, setExp2] = useState<number | null>(null);
+  const [mod2, setMod2] = useState<number | null>(null);
+  const [result2, setResult2] = useState<number | null>(null);
+
+  // States for Form3
+  const [base3, setBase3] = useState<number | null>(null);
+  const [exp3, setExp3] = useState<number | null>(null);
+  const [mod3, setMod3] = useState<number | null>(null);
+  const [result3, setResult3] = useState<number | null>(null);
+
+  // States for Form4
+  const [base4, setBase4] = useState<number | null>(null);
+  const [exp4, setExp4] = useState<number | null>(null);
+  const [mod4, setMod4] = useState<number | null>(null);
+  const [result4, setResult4] = useState<number | null>(null);
+
+  const calculateResult1 = () => {
+    if (base1 === null || exp1 === null || mod1 === null) return;
+    const computedResult = Math.pow(base1, exp1) % mod1;
+    setResult1(computedResult);
+    setExp2(exp1); // Sync exp of Form 2 with Form 1
+    setBase2(computedResult);
+  };
+  const calculateResult2 = () => {
+    if (base2 === null || exp2 === null || mod2 === null || mod2 === 0) return;
+    const computedResult = Math.pow(base2, exp2) % mod2;
+    setResult2(computedResult);
+  };
+  const handleExp1Change = (value: number | null) => {
+    setExp1(value);
   };
 
-  const handleExponentChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(e.target.value);
-    if (!isNaN(value)) setExponent(value);
-    else setExponent(null);
+  const handleExpChange3 = (value: number | null) => {
+    setExp3(value);
+    setExp4(value); // Sync exp of Form 4 with Form 3
   };
-
-  const handleModulusChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(e.target.value);
-    if (!isNaN(value)) setModulus(value);
-    else setModulus(null);
+  const handleModChange3 = (value: number | null) => setMod3(value);
+  const calculateResult3 = () => {
+    if (!base3 || !exp3 || !mod3) return;
+    const computedResult = Math.pow(base3, exp3) % mod3;
+    setResult3(computedResult);
+    setBase4(computedResult); // Set base of Form 4
   };
-
-  const calculateResult = () => {
-    if (!base || !exponent || !modulus) return; // Ensure all values are provided
-
-    const computedResult = Math.pow(base, exponent) % modulus;
-    setResult(computedResult);
+  const calculateResult4 = () => {
+    if (!base4 || !exp4 || !mod4) return;
+    const computedResult = Math.pow(base4, exp4) % mod4;
+    setResult4(computedResult);
+    // Again, set the base of another form here if needed
+    // Example: setBase1(computedResult); // if you want to link back to form 1
   };
-
   return (
     <div>
       <Image src={myGif} alt="Description of GIF" />
-      <div className="calculator">
-        <input
-          type="text"
-          className="formbox-1"
-          value={base || ""}
-          onChange={handleBaseChange}
-        />
-        <span className="operator-1">^</span>
-        <input
-          type="text"
-          className="formbox-2"
-          value={exponent || ""}
-          onChange={handleExponentChange}
-        />
-        <span className="operator-2">%</span>
-        <input
-          type="text"
-          className="formbox-3"
-          value={modulus || ""}
-          onChange={handleModulusChange}
-        />
-        <button className="equals-button" onClick={calculateResult}>
-          =
-        </button>
-        <input
-          type="text"
-          className="formbox"
-          style={{ width: "70px" }} // Inline style for one-off changes
-          readOnly
-          value={result !== null ? result.toString() : ""} // Convert result to string to display
-          placeholder="Result"
-        />
-      </div>
+      <Form
+        base={base1}
+        exp={exp1}
+        mod={mod1}
+        result={result1}
+        onBaseChange={setBase1}
+        onExpChange={handleExp1Change}
+        onModChange={setMod1}
+        onCalculate={calculateResult1}
+      />
+      <Form
+        base={base2}
+        exp={exp2}
+        mod={mod2}
+        result={result2}
+        onBaseChange={setBase2}
+        onExpChange={setExp2}
+        onModChange={setMod2}
+        onCalculate={calculateResult2}
+      />
+      <Form
+        base={base3}
+        exp={exp3}
+        mod={mod3}
+        result={result3}
+        onBaseChange={setBase3}
+        onExpChange={handleExpChange3}
+        onModChange={handleModChange3}
+        onCalculate={calculateResult3}
+      />
+      <Form
+        base={base4}
+        exp={exp4}
+        mod={mod4}
+        result={result4}
+        onBaseChange={setBase4}
+        onExpChange={setExp4}
+        onModChange={setMod4}
+        onCalculate={calculateResult4} // Remember to define calculateResult4
+      />
     </div>
   );
 }
